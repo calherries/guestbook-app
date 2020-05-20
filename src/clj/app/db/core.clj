6dc0fea8fb4b48d2b6582e28795a87e184ecbdf5
Callum Herries <callum.herries@uber.com>
2020-05-20 23:00:03 +0200

    Actually add message route

--- src/clj/app/db/core.clj
+++ src/clj/app/db/core.clj
@@ -1,10 +1,13 @@
 (ns app.db.core
   (:require
-    [next.jdbc.date-time]
-    [next.jdbc.result-set]
-    [conman.core :as conman]
-    [mount.core :refer [defstate]]
-    [app.config :refer [env]]))
+   [clojure.java.jdbc :as jdbc]
+   [java-time :refer [java-date]]
+   [java-time.pre-java8 :as jt]
+   [next.jdbc.date-time]
+   [next.jdbc.result-set]
+   [conman.core :as conman]
+   [mount.core :refer [defstate]]
+   [app.config :refer [env]]))
 
 (defstate ^:dynamic *db*
           :start (conman/connect! {:jdbc-url (env :database-url)})
@@ -12,19 +15,54 @@
 
 (conman/bind-connection *db* "sql/queries.sql")
 
-(extend-protocol next.jdbc.result-set/ReadableColumn
+;; (extend-protocol next.jdbc.result-set/ReadableColumn
+;;   java.sql.Timestamp
+;;   (read-column-by-label [^java.sql.Timestamp v _]
+;;     (.toLocalDateTime v))
+;;   (read-column-by-index [^java.sql.Timestamp v _2 _3]
+;;     (.toLocalDateTime v))
+;;   java.sql.Date
+;;   (read-column-by-label [^java.sql.Date v _]
+;;     (.toLocalDate v))
+;;   (read-column-by-index [^java.sql.Date v _2 _3]
+;;     (.toLocalDate v))
+;;   java.sql.Time
+;;   (read-column-by-label [^java.sql.Time v _]
+;;     (.toLocalTime v))
+;;   (read-column-by-index [^java.sql.Time v _2 _3]
+;;     (.toLocalTime v)))
+
+                                        ;
+(extend-protocol jdbc/IResultSetReadColumn
   java.sql.Timestamp
-  (read-column-by-label [^java.sql.Timestamp v _]
-    (.toLocalDateTime v))
-  (read-column-by-index [^java.sql.Timestamp v _2 _3]
-    (.toLocalDateTime v))
+  (result-set-read-column [v _2 _3]
+    (java-date (.atZone (.toLocalDateTime v) (java.time.ZoneId/systemDefault))))
   java.sql.Date
-  (read-column-by-label [^java.sql.Date v _]
-    (.toLocalDate v))
-  (read-column-by-index [^java.sql.Date v _2 _3]
+  (result-set-read-column [v _2 _3]
     (.toLocalDate v))
   java.sql.Time
-  (read-column-by-label [^java.sql.Time v _]
-    (.toLocalTime v))
-  (read-column-by-index [^java.sql.Time v _2 _3]
+  (result-set-read-column [v _2 _3]
     (.toLocalTime v)))
+                                        ;
+
+(extend-protocol jdbc/ISQLValue
+  java.util.Date
+  (sql-value [v]
+    (java.sql.Timestamp. (.getTime v)))
+  java.time.LocalTime
+  (sql-value [v]
+    (jt/sql-time v))
+  java.time.LocalDate
+  (sql-value [v]
+    (jt/sql-date v))
+  java.time.LocalDateTime
+  (sql-value [v]
+    (jt/sql-timestamp v))
+  java.time.ZonedDateTime
+  (sql-value [v]
+    (jt/sql-timestamp v)))
+
+(comment (get-messages))
+(comment (save-message! {:name "Bob" :message "Hello, world"}))
+(comment (save-message! {:name "Alice" :message "Hello, world"}))
+(comment (save-message! {:name "Roxy" :message "Hello, world"}))
--- src/cljc/app/validation.cljc
+++ src/cljc/app/validation.cljc
@@ -1,2 +1,15 @@
 (ns app.validation
   (:require [struct.core :as st]))
+
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message  "message must be more than 10 characters"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [msg]
+  (first (st/validate msg message-schema)))
